606b36bb7518a876d72151dddcceec3b15f6ba31
Imama Ansari <ansariimama@yahoo.com>
2026-01-13 02:54:01 +0500

    add Hero component for enhanced landing section

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import NavScrollExample from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
       <NavScrollExample />
-
-      <div className="container mt-5">
-        <h1>Imama Ansari</h1>
-        <p>First React Project</p>
-      </div>
+      <Hero />
     </>
   );
 }
